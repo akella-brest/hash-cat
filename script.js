00450519556fbb04ad0bfade8ae20b94760f751e
Maxim Dobrolinsky <maxim.dobrolinsky@gmail.com>
2021-03-10 14:09:31 +0300

    update script

--- script.js
+++ script.js
@@ -8,27 +8,29 @@ function closeContactForm() {
     document.getElementById('blackout-wrapper').setAttribute('style', 'display: none;');
 }
 
-$(document).ready(function () {
-    $('.myform').on('submit', function () {
-
-        // Add text 'loading...' right after clicking on the submit button. 
-        $('.output_message').text('Loading...');
-
-        var form = $(this);
-        $.ajax({
-            url: "email.php",
-            method: form.attr('method'),
-            data: form.serialize(),
-            success: function (result) {
-                if (result == 'success') {
-                    $('.output_message').text('Message Sent!');
-                } else {
-                    $('.output_message').text('Error Sending email!');
+(function($) {
+    $(document).ready(function () {
+        $('.myform').on('submit', function () {
+    
+            // Add text 'loading...' right after clicking on the submit button. 
+            $('.output_message').text('Loading...');
+    
+            var form = $(this);
+            $.ajax({
+                url: "email.php",
+                method: form.attr('method'),
+                data: form.serialize(),
+                success: function (result) {
+                    if (result == 'success') {
+                        $('.output_message').text('Message Sent!');
+                    } else {
+                        $('.output_message').text('Error Sending email!');
+                    }
                 }
-            }
+            });
+    
+            // Prevents default submission of the form after clicking on the submit button. 
+            return false;
         });
-
-        // Prevents default submission of the form after clicking on the submit button. 
-        return false;
     });
-});
+})($);
