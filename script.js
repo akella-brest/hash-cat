7618d733a6a440bd44e897365d92d06e843b91ce
Maxim Dobrolinsky <maxim.dobrolinsky@gmail.com>
2021-03-10 14:08:37 +0300

    move script

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function closeContactForm() {
     document.getElementById('contact-form').setAttribute('style', 'display: none;');
     document.getElementById('blackout-wrapper').setAttribute('style', 'display: none;');
 }
+
+$(document).ready(function () {
+    $('.myform').on('submit', function () {
+
+        // Add text 'loading...' right after clicking on the submit button. 
+        $('.output_message').text('Loading...');
+
+        var form = $(this);
+        $.ajax({
+            url: "email.php",
+            method: form.attr('method'),
+            data: form.serialize(),
+            success: function (result) {
+                if (result == 'success') {
+                    $('.output_message').text('Message Sent!');
+                } else {
+                    $('.output_message').text('Error Sending email!');
+                }
+            }
+        });
+
+        // Prevents default submission of the form after clicking on the submit button. 
+        return false;
+    });
+});
